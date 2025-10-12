537be3ae25a7ad4f0a85a279c2a904696b0fcd93
richieam93 <91011752+richieam93@users.noreply.github.com>
2025-10-12 14:04:54 +0200

    Update const.py

--- custom_components/laufschrift/const.py
+++ custom_components/laufschrift/const.py
@@ -1,9 +1,7 @@
 """Constants for the Laufschrift integration."""
 
 DOMAIN = "laufschrift"
-PLATFORMS = ["sensor", "text", "switch", "select"]
+PLATFORMS = ["text", "switch", "select", "brightness", "speed"]
 
 SERVICE_LAUFSCHRIFT_SET_TEXT = "laufschrift_set_text"
-SERVICE_LAUFSCHRIFT_SET_BRIGHTNESS = "laufschrift_set_brightness"
-SERVICE_LAUFSCHRIFT_SET_SPEED = "laufschrift_set_speed"
 SERVICE_LAUFSCHRIFT_SHUTDOWN = "laufschrift_shutdown"
